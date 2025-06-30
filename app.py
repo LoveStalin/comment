31063779158200240635677c8a68d531304e7427
LoveStalin <thanhnguyenxuan917@gmail.com>
2025-07-01 00:00:26 +0700

    Upload Flask code for comment

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask_cors import CORS
 from pymongo import MongoClient
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://happybirthdayphuonganhiloveu.netlify.app"}})
+CORS(app, resources={r"/*": {"origins": "https://happybirthdayphuonganhiloveu.netlify.app"}}, supports_credentials=True)
 
 # Kết nối MongoDB local hoặc MongoDB Atlas
-client = MongoClient("mongodb+srv://nguyenxuanthanh1112010:<db_password>@cluster0.4jpjums.mongodb.net/comment_db?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient("mongodb+srv://nguyenxuanthanh1112010:<jdjJGZUxMhxtuTY8>@cluster0.4jpjums.mongodb.net/comment_db?retryWrites=true&w=majority&appName=Cluster0")
 db = client["comment_db"]
 collection = db["comments"]
 
